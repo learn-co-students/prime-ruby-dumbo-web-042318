01913b440b116cf93325528673ab50f6fc866d99
ahjuice <ahjuice@users.noreply.github.com>
2018-04-19 08:40:03 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+# Add  code here
+def prime?(int)
+  if int < 1
+    false
+  else
+    int_array = (1..int).to_a
+    int_array.reverse!
+    divisors = []
+    int_array.each do |num|
+      if int % num == 0
+        divisors << num
+      end
+    end
+    divisors == [int, 1]
+  end
+end
